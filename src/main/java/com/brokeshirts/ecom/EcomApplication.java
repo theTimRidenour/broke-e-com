6021b9dca72b2f7b30f001812723cbea72f0f46e
the Tim Ridenour <theTimRidenour@gmail.com>
2019-05-25 00:54:27 -0500

    added 'storage' package with code to upload and display files.

--- src/main/java/com/brokeshirts/ecom/EcomApplication.java
+++ src/main/java/com/brokeshirts/ecom/EcomApplication.java
@@ -1,13 +1,27 @@
 package com.brokeshirts.ecom;
 
+import com.brokeshirts.ecom.storage.StorageProperties;
+import com.brokeshirts.ecom.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class EcomApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcomApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.init();
+		};
+	}
 }
