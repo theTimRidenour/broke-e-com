5e4515133cc90453614990db609b2cac332efac3
the Tim Ridenour <theTimRidenour@gmail.com>
2019-06-04 23:04:47 -0500

    added social login

--- src/main/java/com/brokeshirts/ecom/EcomApplication.java
+++ src/main/java/com/brokeshirts/ecom/EcomApplication.java
@@ -2,15 +2,87 @@ package com.brokeshirts.ecom;
 
 import com.brokeshirts.ecom.storage.StorageProperties;
 import com.brokeshirts.ecom.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
+
+import javax.servlet.Filter;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@EnableOAuth2Client
+@EnableAuthorizationServer
 @EnableConfigurationProperties(StorageProperties.class)
-public class EcomApplication {
+@RestController
+public class EcomApplication extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	OAuth2ClientContext oAuth2ClientContext;
+
+	private Filter ssoFilter() {
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+		filters.add(ssoFilter(facebook(), "/login/facebook"));
+		filters.add(ssoFilter(github(), "/login/github"));
+		filter.setFilters(filters);
+		return filter;
+	}
+
+	private Filter ssoFilter(ClientResources client, String path) {
+		OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
+		OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oAuth2ClientContext);
+		filter.setRestTemplate(template);
+		UserInfoTokenServices tokenServices = new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId());
+		tokenServices.setRestTemplate(template);
+		filter.setTokenServices(tokenServices);
+		return filter;
+	}
+
+	class ClientResources {
+
+		@NestedConfigurationProperty
+		private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+		@NestedConfigurationProperty
+		private ResourceServerProperties resource = new ResourceServerProperties();
+
+		public AuthorizationCodeResourceDetails getClient() {
+			return client;
+		}
+
+		public ResourceServerProperties getResource() {
+			return resource;
+		}
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcomApplication.class, args);
@@ -21,5 +93,69 @@ public class EcomApplication {
 		return (args) -> {
 			storageService.init();
 		};
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook.client")
+	public AuthorizationCodeResourceDetails facebook() {
+		return new AuthorizationCodeResourceDetails();
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook.resource")
+	public ResourceServerProperties facebookResource() {
+		return new ResourceServerProperties();
+	}
+
+	@Bean
+	@ConfigurationProperties("facebook")
+	public ClientResources facebook() {
+		return new ClientResources();
+	}
+
+	@Bean
+	@ConfigurationProperties("github.client")
+	public AuthorizationCodeResourceDetails github() {
+		return new AuthorizationCodeResourceDetails();
+	}
+
+	@Bean
+	@ConfigurationProperties("github.resource")
+	public ResourceServerProperties githubResource() {
+		return new ResourceServerProperties();
+	}
+
+	@Bean
+	@ConfigurationProperties("github")
+	public ClientResources github() {
+		return new ClientResources();
+	}
+
+	@Bean
+	public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+					.antMatchers("/", "/login**", "/error**", "/store**", "/store/**", "/terms", "/policy")
+					.permitAll()
+				.anyRequest()
+					.authenticated()
+					.and()
+				.logout()
+					.logoutSuccessUrl("/")
+					.permitAll()
+					.and()
+				.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+					.and()
+				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
 	}
 }
