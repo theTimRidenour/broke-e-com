98f8778a66277c2d29678b66835f24f38ba51a9a
the Tim Ridenour <theTimRidenour@gmail.com>
2019-06-12 23:20:56 -0500

    fresh start for cookies.

--- src/main/java/com/brokeshirts/ecom/EcomApplication.java
+++ src/main/java/com/brokeshirts/ecom/EcomApplication.java
@@ -5,68 +5,13 @@ import com.brokeshirts.ecom.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-@RestController
 public class EcomApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(EcomApplication.class);
-	}
-
-/*	@Autowired
-	OAuth2ClientContext oAuth2ClientContext;
-
-	private Filter ssoFilter() {
-		CompositeFilter filter = new CompositeFilter();
-		List<Filter> filters = new ArrayList<>();
-		filters.add(ssoFilter(facebook(), "/login/facebook"));
-		filters.add(ssoFilter(github(), "/login/github"));
-		filter.setFilters(filters);
-		return filter;
-	}
-
-	private Filter ssoFilter(ClientResources client, String path) {
-		OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
-		OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oAuth2ClientContext);
-		filter.setRestTemplate(template);
-		UserInfoTokenServices tokenServices = new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId());
-		tokenServices.setRestTemplate(template);
-		filter.setTokenServices(tokenServices);
-		return filter;
-	}
-
-	class ClientResources {
-
-		@NestedConfigurationProperty
-		private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
-
-		@NestedConfigurationProperty
-		private ResourceServerProperties resource = new ResourceServerProperties();
-
-		public AuthorizationCodeResourceDetails getClient() {
-			return client;
-		}
-
-		public ResourceServerProperties getResource() {
-			return resource;
-		}
-	} */
-
-	@RequestMapping("/user")
-	public Principal user(Principal principal) {
-
-		return principal; }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcomApplication.class, args);
@@ -78,47 +23,4 @@ public class EcomApplication extends SpringBootServletInitializer {
 			storageService.init();
 		};
 	}
-
-/*	@Bean
-	@ConfigurationProperties("facebook")
-	public ClientResources facebook() {
-		return new ClientResources();
-	}
-
-	@Bean
-	@ConfigurationProperties("github")
-	public ClientResources github() {
-		return new ClientResources();
-	}
-
-	@Bean
-	public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-		FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
-		registration.setFilter(filter);
-		registration.setOrder(-100);
-		return registration;
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.antMatcher("/**")
-				.authorizeRequests()
-					.antMatchers("/", "/login**", "/error**", "/store**", "/store/**", "/terms", "/policy", "/css/**", "/images/**", "/upload/**")
-					.permitAll()
-				.anyRequest()
-					.authenticated()
-					.and()
-				.exceptionHandling()
-					.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
-					.and()
-				.logout()
-					.logoutSuccessUrl("/")
-					.permitAll()
-					.and()
-				.csrf()
-					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-					.and()
-				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-	} */
 }
